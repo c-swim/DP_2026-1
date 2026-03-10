4bb268ccdaedc214a55a6125a8890db52f7dd682
reco <reco27172@ds.ac.kr>
2026-03-10 11:46:38 +0900

    3월10일 수업종료

---
+++ src/practice/ch01/Main.java
@@ -0,0 +1,7 @@
+package practice.ch01;
+
+public class Main{
+    public static void main(String[] args){
+        Book book = new Book(name: "Java Programing");
+        System.out.println("Book name: " + book.getName());
+    }
