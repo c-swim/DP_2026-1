dd8c51f266854b9abb95af20ad90def6694c6ec9
reco <reco27172@ds.ac.kr>
2026-03-12 10:16:44 +0900

    3월12일 수업종료

--- src/practice/ch01/BookShelfIterator.java
+++ src/practice/ch01/BookShelfIterator.java
@@ -1,32 +1,32 @@
 package practice.ch01;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.Iterator; // 인터페이스, 제너릭
+import java.util.NoSuchElementException; 
 
 public class BookShelfIterator implements Iterator<Book> {
     private BookShelf bookShelf;
-    private int index;
+    private int index;  // 다음에 꺼내올 책의 위치를 저장하는 필드
 
     public BookShelfIterator(BookShelf bookShelf) {
-        this.bookShelf = bookShelf;
-        this.index = 0;
+        this.bookShelf = bookShelf; // 자기가 돌아다닐 책꽂이를 저장하는 필드
+        this.index = 0; 
     }
 
     @Override
-    public boolean hasNext() {
+    public boolean hasNext() {  // 다음 책이 있는지 여부를 반환하는 메소드
         if (index < bookShelf.getLength()) {
-            return true;
+            return true;    // 꺼내올 책이 남아있음
         } else {
-            return false;
+            return false;   // 꺼내올 책이 더 이상 없음
         }
     }
 
     @Override
-    public Book next() {
+    public Book next() {    // 다음 책을 반환하는 메소드
         if (!hasNext()) {
-            throw new NoSuchElementException();
+            throw new NoSuchElementException(); // 더 이상 책이 없으며 예외를 던짐
         }
-        Book book = bookShelf.getBookAt(index);
+        Book book = bookShelf.getBookFrom(index); // 반복자가 책꽂이에 접근 //Iterator 패턴, getBoookAt -> getBookFrom
         index++;
         return book;
     }
