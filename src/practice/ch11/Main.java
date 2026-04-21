a1c1b9682c7d1b225c7b5b4ebbf1133ac54553ee
reco <reco27172@ds.ac.kr>
2026-04-21 11:22:37 +0900

    4월 21일 수업종료

--- src/practice/ch11/Main.java
+++ src/practice/ch11/Main.java
@@ -2,31 +2,26 @@ package practice.ch11;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Making root entries...");
-        Directory rootdir = new Directory("root");
-        Directory bindir = new Directory("bin");
-        Directory tmpdir = new Directory("tmp");
-        Directory usrdir = new Directory("usr");
-        rootdir.add(bindir);
-        rootdir.add(tmpdir);
-        rootdir.add(usrdir);
-        bindir.add(new File("vi", 10000));
-        bindir.add(new File("latex", 20000));
-        rootdir.printList();
-        System.out.println();
+        // 디렉토리 트리
+        Entry root = new Directory("root"); //업 캐스팅
+        
+        Directory bin = new Directory("bin");
+        Directory tmp = new Directory("tmp");
+        Directory usr = new Directory("usr");
 
-        System.out.println("Making user entries...");
-        Directory youngjin = new Directory("youngjin");
-        Directory gildong = new Directory("gildong");
-        Directory dojun = new Directory("dojun");
-        usrdir.add(youngjin);
-        usrdir.add(gildong);
-        usrdir.add(dojun);
-        youngjin.add(new File("diary.html", 100));
-        youngjin.add(new File("Composite.java", 200));
-        gildong.add(new File("memo.tex", 300));
-        dojun.add(new File("game.doc", 400));
-        dojun.add(new File("junk.mail", 500));
-        rootdir.printList();
+        
+        root.add(bin);
+        root.add(tmp);
+        root.add(usr);
+
+        // 다운 캐스팅
+        
+        bin.add(new File("vi", 10000));
+        bin.add(new File("latex", 20000));
+
+        // 경로 출력
+        root.printList();
+        
+        
     }
 }
