3019d81d62cf8048d0874c3e10c9ad64492ab5ff
reco <reco27172@ds.ac.kr>
2026-05-21 02:20:51 +0900

    hw17.ver1

--- src/hw/ch16/Main.java
+++ src/hw/ch16/Main.java
@@ -2,6 +2,7 @@ package hw.ch16;
 
 public class Main {
     static public void main(String[] args) {
+        System.out.println("\n20240877 최영진\n");
         new LoginFrame("Mediator Sample");
     }
 }
--- src/hw/ch17/Main.java
+++ src/hw/ch17/Main.java
@@ -2,6 +2,7 @@ package hw.ch17;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\n20240877 최영진\n");
         NumberGenerator generator = new EvenNumberGenerator(0, 30);
         
         Observer observer1 = new DigitObserver();
